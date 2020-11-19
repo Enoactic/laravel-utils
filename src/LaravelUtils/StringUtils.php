4db1da30cd0d8a4514ae1685b8dc2329b73dd3a4
Boonyaphat Intasin <aosmosiss-ze@hotmail.com>
2020-11-19 14:16:52 +0700

    - Initial Packages

---
+++ src/LaravelUtils/StringUtils.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace LaravelUtils;
+
+class StringUtils
+{
+    public static function stringToWordArray($string, $length = 76)
+    {
+        $return = array();
+        $stringArray = array_chunk(preg_split("//u", $string, -1, PREG_SPLIT_NO_EMPTY), $length);
+        foreach ($stringArray as $string) {
+            $return[] = join("", $string);
+        }
+        return $return;
+    }
+
+    public static function stringToSlug($text)
+    {
+        $text = strtolower($text);
+        return preg_replace('/[^A-Za-z0-9ก-๙\-]/u', '-',str_replace('&', '-and-', $text));
+    }
+
+    public static function stringToYoutubeEmbed($url)
+    {
+        $shortUrlRegex = '/youtu.be\/([a-zA-Z0-9_-]+)\??/i';
+        $longUrlRegex = '/youtube.com\/((?:embed)|(?:watch))((?:\?v\=)|(?:\/))([a-zA-Z0-9_-]+)/i';
+
+        if (preg_match($longUrlRegex, $url, $matches)) {
+            $youtube_id = $matches[count($matches) - 1];
+        }
+
+        if (preg_match($shortUrlRegex, $url, $matches)) {
+            $youtube_id = $matches[count($matches) - 1];
+        }
+        return 'https://www.youtube.com/embed/' . $youtube_id ;
+    }
+}
